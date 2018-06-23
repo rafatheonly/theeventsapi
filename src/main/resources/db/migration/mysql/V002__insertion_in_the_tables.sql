11f93bb7a09c121af1de9b3965f2cfe02eb906ee
Rafael Calearo <rafaelcalearo@hotmail.com>
2018-06-22 21:03:23 -0300

    :pencil2: — Renomeando arquivos da pasta de Testes.

--- src/main/resources/db/migration/mysql/V002__insertion_in_the_tables.sql
+++ src/main/resources/db/migration/mysql/V002__insertion_in_the_tables.sql
@@ -1,5 +1,6 @@
 INSERT INTO usuario (id, nome, rg, email, senha, foto, perfil, ativo) 
-VALUES (1, "Rafael Calearo", "1091507911", "rafaelcalearo@theevents.com", '$2a$10$8Pc1KGsm3tR1x9vHpolQLuk8iGaNUzvNoubnt1mA7x4OPabXipaOW', "-", "ROLE_ADMIN", "0");
+VALUES (1, "Rafael Calearo", "1091507911", "rafaelcalearo@theevents.com", '$2a$10$8Pc1KGsm3tR1x9vHpolQLuk8iGaNUzvNoubnt1mA7x4OPabXipaOW', "-", "ROLE_ADMIN", "1");
+VALUES (2, "michel Luz", "1091507922", "michelluz@theevents.com", '$2a$10$8Pc1KGsm3tR1x9vHpolQLuk8iGaNUzvNoubnt1mA7x4OPabXipaOW', "-", "ROLE_ADMIN", "1");
 
 INSERT INTO tipoevento (id, descricao_tipo_evento, usuario_id) VALUES (1, "Festa Junina", 1);
 INSERT INTO tipoevento (id, descricao_tipo_evento, usuario_id) VALUES (2, "Festa Country", 1);
--- src/test/java/com/theeventsapi/TheeventsapiApplicationTests.java
+++ src/test/java/com/theeventsapi/TheeventsapiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.theeventsspringboot;
+package com.theeventsapi;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class TheeventsspringbootApplicationTests {
+public class TheeventsapiApplicationTests {
 
 	@Test
 	public void contextLoads() {
